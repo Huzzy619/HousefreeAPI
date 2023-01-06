a465ec3d36566db77b77bebf5d0de1be686943cf
Joshua <cutejosh2@gmail.com>
2023-01-06 08:32:55 +0100

    blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-	
+    path('blogs/', views.BlogListView.as_view(), name='blog-list'),
+    # path('blogs/create/', views.BlogCreateView.as_view(), name='blog-create'),
+    path('blogs/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail'),
+    # path('blogs/<int:pk>/update/', views.BlogUpdateView.as_view(), name='blog-update'),
+    # path('blogs/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='blog-delete'),
 ]
