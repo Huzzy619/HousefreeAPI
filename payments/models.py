7eab9f4cbd3dec6d7d5fa1619271f4bd9683e7ea
Joshua <cutejosh2@gmail.com>
2023-01-13 12:00:45 +0100

    updated payment model

--- payments/models.py
+++ payments/models.py
@@ -2,22 +2,30 @@ from django.db import models
 from django.contrib.auth import get_user_model
 import secrets
 
+PAYMENT_OPTION_CHOICES = (
+	('paystack', 'paystack'),
+	('flutterwave', 'flutterwave'),
+)
+
 class Payment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
-    email = models.EmailField()
-    amount = models.PositiveIntegerField()
-    txn_ref = models.CharField(max_length=200, unique=True)
-    verified = models.BooleanField(default=False)
-    date_created = models.DateTimeField(auto_now_add=True)
+	user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
+	email = models.EmailField()
+	amount = models.FloatField()
+	txn_ref = models.CharField(max_length=200, unique=True)
+	verified = models.BooleanField(default=False)
+	payment_options = models.CharField(max_length=255, choices=PAYMENT_OPTION_CHOICES)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	metadata = models.JSONField()
 
-    class Meta:
-        ordering = ('-date_created',)
-        verbose_name_plural = 'Payments'
+	class Meta:
+		ordering = ('-date_created',)
+		verbose_name_plural = 'Payments'
 
-    def __str__(self):
-        return f"Payment of {self.amount} by {self.user} on {self.date_created}"
+	def __str__(self):
+		return f"Payment of {self.amount} by {self.user} on {self.created_at}"
 
-    def save(self, *args, **kwargs):
-        if not self.txn_ref:
-            self.txn_ref = secrets.token_urlsafe(50)
-        super().save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		if not self.txn_ref:
+			self.txn_ref = secrets.token_urlsafe(50)
+		super().save(*args, **kwargs)
