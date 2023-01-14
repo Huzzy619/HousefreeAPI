fc9c4f00bb339a052a644402b1bd70b3c6836279
Joshua <cutejosh2@gmail.com>
2023-01-14 09:50:23 +0100

    wrote method to initialize a paystack payment object

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,19 +1,51 @@
 from rest_framework import serializers
 from .models import Payment
+from django.contrib.auth.models import User
+from django.conf import settings
+import requests
 
 class PaymentSerializer(serializers.ModelSerializer):
-    metadata = serializers.JSONField(required=False)
+	metadata = serializers.JSONField(required=False)
 
-    def validate_metadata(self, value):
-        if not isinstance(value, dict):
-            raise serializers.ValidationError("metadata must be a dictionary.")
-        if 'product_name' not in value:
-            raise serializers.ValidationError("metadata must contain a product_name key.")
-        if len(value['product_name']) > 100:
-            raise serializers.ValidationError("product_name must be less than 100 characters.")
-        return value
+	def validate_metadata(self, value):
+		if not isinstance(value, dict):
+			raise serializers.ValidationError("metadata must be a dictionary.")
+		if 'product_name' not in value:
+			raise serializers.ValidationError("metadata must contain a product_name key.")
+		if len(value['product_name']) > 100:
+			raise serializers.ValidationError("product_name must be less than 100 characters.")
+		return value
 
-    class Meta:
-        model = Payment
-        fields = ('id', 'user', 'email', 'amount', 'txn_ref', 'verified', 'payment_options', 'created_at', 'updated_at', 'metadata')
-        read_only_fields = ('txn_ref', 'created_at', 'updated_at')
+	class Meta:
+		model = Payment
+		fields = ('id', 'user', 'email', 'amount', 'txn_ref', 'verified', 'payment_options', 'created_at', 'updated_at', 'metadata')
+		read_only_fields = ('txn_ref', 'created_at', 'updated_at')
+
+class PaystackPaymentSerializer(serializers.ModelSerializer):
+	amount = serializers.IntegerField()
+	email = serializers.EmailField()
+
+	def validate_email(self, value):
+		if User.objects.filter(email=value).exists():
+			return value
+		raise serializers.ValidationError({"detail": "Email not found"})
+
+	def save(self):
+		user = self.context['request'].user
+		data = self.validated_data
+		url = 'https://api.paystack.co/transaction/initialize'
+		headers = {
+			"Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
+			"Content-Type": "application/json"
+		}
+		response = requests.post(url, headers=headers, json=data)
+		response_data = response.json()
+		if response.status_code != 200:
+			raise Exception(response_data['message'])
+		Payment.objects.create(
+			user=user,
+			amount=data['amount'],
+			email=user.email,
+			verified=False
+		)
+		return response_data
